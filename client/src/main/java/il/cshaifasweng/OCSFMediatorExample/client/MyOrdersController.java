bba7a550e246ed2900eb98e003cc742eee1ab71b
BoshraDh <boshra2003dhamshy@gmail.com>
2026-01-21 17:38:14 +0200

    Link orders page to complaint submission

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/MyOrdersController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/MyOrdersController.java
@@ -50,9 +50,6 @@ public class MyOrdersController {
     @FXML // fx:id="backToCatalog"
     private Button backToCatalog; // Value injected by FXMLLoader
 
-    @FXML // fx:id="complaintText"
-    private TextField complaintText; // Value injected by FXMLLoader
-
     @FXML // fx:id="creditCVV"
     private TextField creditCVV; // Value injected by FXMLLoader
 
@@ -80,8 +77,8 @@ public class MyOrdersController {
     @FXML // fx:id="greetingText"
     private TextField greetingText; // Value injected by FXMLLoader
 
-    @FXML // fx:id="openComplaint"
-    private Button openComplaint; // Value injected by FXMLLoader
+    @FXML // fx:id="submitComplaint"
+    private Button submitComplaint; // Value injected by FXMLLoader
 
     @FXML // fx:id="orderID"
     private TextField orderID; // Value injected by FXMLLoader
@@ -91,9 +88,6 @@ public class MyOrdersController {
 
     @FXML // fx:id="refresh"
     private Button refresh; // Value injected by FXMLLoader
-
-    @FXML // fx:id="sendComplaint"
-    private Button sendComplaint; // Value injected by FXMLLoader
 
     @FXML // fx:id="shopID"
     private TextField shopID; // Value injected by FXMLLoader
@@ -236,52 +230,28 @@ public class MyOrdersController {
 
 
     @FXML
-    void openOrderComplaint(ActionEvent event)
-    {
-        complaintText.setVisible(true);
-        sendComplaint.setVisible(true);
-    }
+    void goToMyComplaints(ActionEvent event) {
+        NavigationService.getInstance().navigate("mycomplaints");
 
-    @FXML
-    void sendOrderComplaint(ActionEvent event)
-    {
-        Calendar calle = Calendar.getInstance();
-        int currentYear = calle.get(Calendar.YEAR);
-        int currentMonth = calle.get(Calendar.MONTH);
-        currentMonth++;
-        int currentHour = calle.get(Calendar.HOUR_OF_DAY);
-        int currentMintue = calle.get(Calendar.MINUTE);
-        int currentDay = calle.get(Calendar.DAY_OF_MONTH);
+        PassAccountEventComplaints recievedAcc = new PassAccountEventComplaints(currentUser);
+        new java.util.Timer().schedule(
+                new java.util.TimerTask() {
+                    @Override
+                    public void run() {
+                        EventBus.getDefault().post(recievedAcc);
+                        System.out.println("the server sent me the account , NICE 4 !!");
+                    }
+                },4000
+        );
 
-        Complaint newComplaint = new Complaint();
-        newComplaint.setComplaintID(0); // 0 - Will be changed later
-        newComplaint.setCustomerID(currentUser.getAccountID());
-        newComplaint.setOrderID(SelectedOrder.getOrderID());
-        newComplaint.setAccepted(false);
-        newComplaint.setIn24Hours(false);
-        newComplaint.setComplaintText(complaintText.getText());
-        newComplaint.setShopID(SelectedOrder.getShopID());
-        newComplaint.setAnswerworkerID(0);
-        newComplaint.setReturnedMoney(false);
-        newComplaint.setReturnedmoneyvalue(0);
-        newComplaint.setDay(currentDay);
-        newComplaint.setMonth(currentMonth);
-        newComplaint.setYear(currentYear);
-        newComplaint.setReplyText("");
-        sendComplaint.setVisible(false);
-        complaintText.setVisible(false);
-        UpdateMessage new_msg=new UpdateMessage("complaint","add");
-        new_msg.setComplaint(newComplaint);
+        GetAllComplaints allComplaints = new GetAllComplaints();
+        System.out.println("send request for complaints !!");
         try {
-            System.out.println("before sending updateMessage to server ");
-            SimpleClient.getClient().sendToServer(new_msg); // sends the updated product to the server class
-            System.out.println("afater sending updateMessage to server ");
+            System.out.println("before sending the getAllComplaints " );
+            SimpleClient.getClient().sendToServer(allComplaints);
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
-
     }
 
     Order retrievedOrder = new Order();  // This is the order with theID
@@ -334,7 +304,7 @@ public class MyOrdersController {
 
             viewOrder.setVisible(true);  // Value injected by FXMLLoader
 
-            openComplaint.setVisible(true);  // Value injected by FXMLLoader
+            submitComplaint.setVisible(true);  // Value injected by FXMLLoader
 
             orderProducts.setVisible(true);
 
@@ -370,7 +340,7 @@ public class MyOrdersController {
                 else
                     currentProduct = "";
             }
-            openComplaint.setVisible(true);
+            submitComplaint.setVisible(true);
             orderID.setText(String.valueOf(retrievedOrder.getOrderID()));
             accountID.setText(String.valueOf(currentUser.getAccountID()));
             creditNumber.setText(String.valueOf(retrievedOrder.getCreditCardNumber()));
@@ -423,7 +393,6 @@ public class MyOrdersController {
         assert RecepNumber != null : "fx:id=\"RecepNumber\" was not injected: check your FXML file 'myorders.fxml'.";
         assert accountID != null : "fx:id=\"accountID\" was not injected: check your FXML file 'myorders.fxml'.";
         assert backToCatalog != null : "fx:id=\"backToCatalog\" was not injected: check your FXML file 'myorders.fxml'.";
-        assert complaintText != null : "fx:id=\"complaintText\" was not injected: check your FXML file 'myorders.fxml'.";
         assert creditCVV != null : "fx:id=\"creditCVV\" was not injected: check your FXML file 'myorders.fxml'.";
         assert creditExpire != null : "fx:id=\"creditExpire\" was not injected: check your FXML file 'myorders.fxml'.";
         assert creditNumber != null : "fx:id=\"creditNumber\" was not injected: check your FXML file 'myorders.fxml'.";
@@ -433,11 +402,10 @@ public class MyOrdersController {
         assert deliverStatus != null : "fx:id=\"deliverStatus\" was not injected: check your FXML file 'myorders.fxml'.";
         assert gift != null : "fx:id=\"gift\" was not injected: check your FXML file 'myorders.fxml'.";
         assert greetingText != null : "fx:id=\"greetingText\" was not injected: check your FXML file 'myorders.fxml'.";
-        assert openComplaint != null : "fx:id=\"openComplaint\" was not injected: check your FXML file 'myorders.fxml'.";
         assert orderID != null : "fx:id=\"orderID\" was not injected: check your FXML file 'myorders.fxml'.";
         assert orderList != null : "fx:id=\"orderList\" was not injected: check your FXML file 'myorders.fxml'.";
         assert refresh != null : "fx:id=\"refresh\" was not injected: check your FXML file 'myorders.fxml'.";
-        assert sendComplaint != null : "fx:id=\"sendComplaint\" was not injected: check your FXML file 'myorders.fxml'.";
+        assert submitComplaint != null : "fx:id=\"submitComplaint\" was not injected: check your FXML file 'myorders.fxml'.";
         assert shopID != null : "fx:id=\"shopID\" was not injected: check your FXML file 'myorders.fxml'.";
         assert text1 != null : "fx:id=\"text1\" was not injected: check your FXML file 'myorders.fxml'.";
         assert text10 != null : "fx:id=\"text10\" was not injected: check your FXML file 'myorders.fxml'.";
@@ -462,8 +430,6 @@ public class MyOrdersController {
 
 
         wait.setVisible(true);
-        complaintText.setVisible(false);
-        sendComplaint.setVisible(false);
         cancelButton.setVisible(false);
 
         viewOrder.setText("Load Orders");
@@ -485,8 +451,7 @@ public class MyOrdersController {
         orderID.setVisible(false);  // Value injected by FXMLLoader
         shopID.setVisible(false);  // Value injected by FXMLLoader
         totalPrice.setVisible(false);  // Value injected by FXMLLoader
-        complaintText.setVisible(false);  // Value injected by FXMLLoader
-        openComplaint.setVisible(false);  // Value injected by FXMLLoader
+        submitComplaint.setVisible(false);  // Value injected by FXMLLoader
         orderProducts.setVisible(false);
 
         viewOrder.setDisable(true);
