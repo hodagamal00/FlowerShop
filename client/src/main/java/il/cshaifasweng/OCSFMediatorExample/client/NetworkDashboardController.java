13acae2b382dbf5d5231dae5672206f270251027
BoshraDh <boshra2003dhamshy@gmail.com>
2026-01-23 16:10:57 +0200

    Fix reply complaint label width

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/NetworkDashboardController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/NetworkDashboardController.java
@@ -14,6 +14,7 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
